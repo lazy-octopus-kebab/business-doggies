eabe4a6746c0041400d058f7c6f0d363bf834a05
ShviXXL <shvix@pm.me>
2020-12-23 19:05:10 +0300

    Fix reviews

--- reviews/views.py
+++ reviews/views.py
@@ -1,11 +1,9 @@
 from django.shortcuts import render
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
-from django.views.generic import (
-	ListView,
-	CreateView,
-	DeleteView
-)
+from django.views.generic import ListView, CreateView, DeleteView
+
 from .models import Review, ReviewRating
+
 
 class ReviewListView(ListView):
 	model = Review
@@ -13,6 +11,7 @@ class ReviewListView(ListView):
 	template_name = 'reviews/profile.html'
 	context_object_name = 'reviews'
 	
+
 class ReviewCreateView(LoginRequiredMixin, CreateView):
 	model = Review
 	#<app>/<model>_<viewtype>.html
@@ -22,17 +21,17 @@ class ReviewCreateView(LoginRequiredMixin, CreateView):
 		form.instance.author = self.request.user
 		return super().form_valid(form)
 
+
 class ReviewDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
 	model = Review
 	success_url = 'reviews/profile.html'
 	#<app>/<model>_<viewtype>.html
 
-    def test_func(self):
-        review = self.get_object()
-        if self.request.user == review.author:
-            return True
-        return False
-
+	def test_func(self):
+		review = self.get_object()
+		if self.request.user == review.author:
+			return True
+		return False
 
 
 class ReviewRatingListView(ListView):
@@ -41,6 +40,7 @@ class ReviewRatingListView(ListView):
 	template_name = 'reviews/profile.html'
 	context_object_name = 'reviews_rating'
 	
+
 class ReviewRatingCreateView(LoginRequiredMixin, CreateView):
 	model = ReviewRating
 	#<app>/<model>_<viewtype>.html
@@ -50,14 +50,15 @@ class ReviewRatingCreateView(LoginRequiredMixin, CreateView):
 		form.instance.author = self.request.user
 		return super().form_valid(form)
 
+
 class ReviewRatingDeleteView(DeleteView):
 	model = ReviewRating
 	success_url = 'reviews/profile.html'
 	#<app>/<model>_<viewtype>.html
 
-    def test_func(self):
-        review = self.get_object()
-        if self.request.user == review.author:
-            return True
-        return False
+	def test_func(self):
+		review = self.get_object()
+		if self.request.user == review.author:
+			return True
+		return False
 
