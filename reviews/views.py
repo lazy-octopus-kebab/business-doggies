66077ec06389575879897fb951e42721077b77a0
ShviXXL <shvix@pm.me>
2020-12-23 19:06:29 +0300

    Fix reviews

--- reviews/views.py
+++ reviews/views.py
@@ -1,4 +1,3 @@
-from django.shortcuts import render
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.views.generic import ListView, CreateView, DeleteView
 
@@ -6,59 +5,59 @@ from .models import Review, ReviewRating
 
 
 class ReviewListView(ListView):
-	model = Review
-	#<app>/<model>_<viewtype>.html
-	template_name = 'reviews/profile.html'
-	context_object_name = 'reviews'
-	
+    model = Review
+    #<app>/<model>_<viewtype>.html
+    template_name = 'reviews/profile.html'
+    context_object_name = 'reviews'
+    
 
 class ReviewCreateView(LoginRequiredMixin, CreateView):
-	model = Review
-	#<app>/<model>_<viewtype>.html
-	fields = ['text']
+    model = Review
+    #<app>/<model>_<viewtype>.html
+    fields = ['text']
 
-	def form_valid(self, form):
-		form.instance.author = self.request.user
-		return super().form_valid(form)
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
 
 
 class ReviewDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
-	model = Review
-	success_url = 'reviews/profile.html'
-	#<app>/<model>_<viewtype>.html
+    model = Review
+    success_url = 'reviews/profile.html'
+    #<app>/<model>_<viewtype>.html
 
-	def test_func(self):
-		review = self.get_object()
-		if self.request.user == review.author:
-			return True
-		return False
+    def test_func(self):
+        review = self.get_object()
+        if self.request.user == review.author:
+            return True
+        return False
 
 
 class ReviewRatingListView(ListView):
-	model = ReviewRating
-	#<app>/<model>_<viewtype>.html
-	template_name = 'reviews/profile.html'
-	context_object_name = 'reviews_rating'
-	
+    model = ReviewRating
+    #<app>/<model>_<viewtype>.html
+    template_name = 'reviews/profile.html'
+    context_object_name = 'reviews_rating'
+    
 
 class ReviewRatingCreateView(LoginRequiredMixin, CreateView):
-	model = ReviewRating
-	#<app>/<model>_<viewtype>.html
-	fields = ['text', 'rating']
+    model = ReviewRating
+    #<app>/<model>_<viewtype>.html
+    fields = ['text', 'rating']
 
-	def form_valid(self, form):
-		form.instance.author = self.request.user
-		return super().form_valid(form)
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
 
 
 class ReviewRatingDeleteView(DeleteView):
-	model = ReviewRating
-	success_url = 'reviews/profile.html'
-	#<app>/<model>_<viewtype>.html
+    model = ReviewRating
+    success_url = 'reviews/profile.html'
+    #<app>/<model>_<viewtype>.html
 
-	def test_func(self):
-		review = self.get_object()
-		if self.request.user == review.author:
-			return True
-		return False
+    def test_func(self):
+        review = self.get_object()
+        if self.request.user == review.author:
+            return True
+        return False
 
