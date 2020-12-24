eb3ca8cf6a7c1c6c6915268b867d89376ced33b4
ShviXXL <shvix@pm.me>
2020-12-24 14:30:54 +0300

    Pets urls

--- pets/urls.py
+++ pets/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 
 from .views import (
-    
+    PetCreateView
 )
 
 app_name = 'offers'
 urlpatterns = [
-    # ex: /pets/create
-    path('create/', OfferListView.as_view(), name='create'),
+    # ex: /pets/create/
+    path('create/', PetCreateView.as_view(), name='create'),
 ]
