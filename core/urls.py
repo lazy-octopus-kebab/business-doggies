ac9c97b7788aa2c61bc5535fbf1bbe15fcda1116
ShviXXL <shvix@pm.me>
2020-12-24 04:13:26 +0300

    Some urls configurations

--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 from django.views.generic import TemplateView
 
+from accounts.views import SitterListView
 
 app_name = 'core'
 urlpatterns = [
     # ex: /
     path('', TemplateView.as_view(template_name='core/index.html'), name='index'),
-    path('test_404', TemplateView.as_view(template_name='404.html'), name='404'),
+
+    # ex: /sitters/
+    path('sitters/', SitterListView.as_view(template_name='core/sitters.html'), name='sitters'),
 ]
