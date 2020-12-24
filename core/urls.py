b30fb5d3d20c7eed16f75d50ab9f000604b11a20
Andrey3107 <35701740+Andrey3107@users.noreply.github.com>
2020-12-24 03:43:15 +0300

    Add templates for registration

--- core/urls.py
+++ core/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     # ex: /
     path('', TemplateView.as_view(template_name='core/index.html'), name='index'),
     path('test_404', TemplateView.as_view(template_name='404.html'), name='404'),
+    path('test_btstrp', TemplateView.as_view(template_name='test_bootstrap.html'), name='bootstrap'),
 ]
