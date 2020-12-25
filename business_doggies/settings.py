7696ab0e23008413a11ff712daa035b03709d308
ShviXXL <shvix@pm.me>
2020-12-25 22:51:03 +0300

    Configured docker files

--- business_doggies/settings.py
+++ business_doggies/settings.py
@@ -143,8 +143,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = BASE_DIR
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
 
 # Substituting a custom User model
 AUTH_USER_MODEL = 'accounts.User'
