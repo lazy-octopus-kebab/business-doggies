968f987587c198d9d8d8b58b0f27988663353650
ShviXXL <shvix@pm.me>
2020-12-29 16:25:28 +0300

    Better Users table for admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,28 +1,96 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as DefaultUserAdmin
 
+from allauth.account.models import EmailAddress
+
+from .forms import UserSignUpForm
 from .models import User, Sitter, Client
 
 
-class UserAdmin(admin.ModelAdmin):
-    list_display = ('email', 'phone_number', 'first_name', 'last_name', 'id')
+class ReadOnlyInline(admin.StackedInline):
+    def has_add_permission(self, request, obj=None):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+
+class EmailAddressInline(ReadOnlyInline):
+    model = EmailAddress
+    fields = ('email', 'verified',)
+
+    readonly_fields = ('email', 'verified')
+
+
+class ClientInline(ReadOnlyInline):
+    model = Client
+
+
+class SitterInline(ReadOnlyInline):
+    model = Sitter
+    fields = ('description',)
+
+
+class UserAdmin(DefaultUserAdmin):
+    inlines = (EmailAddressInline,)
+    client_inlines = (ClientInline,)
+    sitter_inlines = (SitterInline,)
+
+    list_display = (
+        'email',
+        'phone_number',
+        'first_name',
+        'last_name',
+        'is_client',
+        'is_sitter',
+        'is_superuser',
+        'id',
+    )
+    list_filter = ('is_client', 'is_sitter')
+    ordering = ('email',)
+
+    readonly_fields = (
+        'date_joined',
+        'is_client',
+        'is_sitter',
+        'last_login',
+        'is_superuser',
+    )
 
     fieldsets = (
-        (None, {'fields': ('email', 'password')}),
-        ('Personal info', {'fields': ('first_name', 'last_name')}),
-        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser',
-                                       'groups', 'user_permissions', 'is_client',
-                                       'is_sitter')}),
-        ('Important dates', {'fields': ('last_login', 'date_joined')}),
-    )
-    add_fieldsets = (
         (None, {
-            'classes': ('wide',),
-            'fields': ('email', 'password1', 'password2'),
+            'fields': ('email', 'phone_number', 'password')
+        }),
+        ('Personal info', {
+            'fields': ['first_name', 'last_name', 'image']
+        }),
+        ('Status', {
+            'fields': (
+                'is_active',
+                'is_client',
+                'is_sitter',
+                'is_superuser',
+            )
+        }),
+        ('Important dates', {
+            'fields': (
+                'last_login',
+                'date_joined',
+            )
         }),
     )
+    
     search_fields = ['email', 'first_name', 'last_name', 'phone_number']
+
+    def has_add_permission(self, request):
+        return False
+
+    def get_inlines(self, request, obj=None):
+        if obj.is_client:
+            return self.inlines + self.client_inlines
+        elif obj.is_sitter:
+            return self.inlines + self.sitter_inlines
+        return self.inlines
 
 
 admin.site.register(User, UserAdmin)
-admin.site.register(Sitter)
-admin.site.register(Client)
--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group
 
-# Register your models here.
+from allauth.account.models import EmailAddress, EmailConfirmation
+from allauth.account import app_settings as allauth_settings
+
+
+admin.site.unregister(Group)
+admin.site.unregister(EmailAddress)
+if not allauth_settings.EMAIL_CONFIRMATION_HMAC:
+    admin.site.unregister(EmailConfirmation)
