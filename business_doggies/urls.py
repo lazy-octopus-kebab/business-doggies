2541b1d314442185ac730bec90466b79c838f082
ShviXXL <shvix@pm.me>
2020-12-29 16:25:05 +0300

    Add urls for allauth

--- business_doggies/urls.py
+++ business_doggies/urls.py
@@ -22,6 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
+    path('accounts/', include('allauth.urls')),
     path('offers/', include('offers.urls')),
     path('reviews/', include('reviews.urls')),
     path('pets/', include('pets.urls')),
